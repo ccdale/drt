c966bdaa07b88c213dcb578b63ad8d7f08579b7c
Chris Allison <chris.charles.allison@gmail.com>
2018-03-08 16:26:48 +0000

    bumping build version

--- drt/__init__.py
+++ drt/__init__.py
@@ -17,6 +17,6 @@
 #     along with drt.  If not, see <http://www.gnu.org/licenses/>.
 majorv = 1
 minorv = 0
-buildv = 18
+buildv = 19
 versionl = (majorv, minorv, buildv)
 __version__ = ".".join(str(x) for x in versionl)
